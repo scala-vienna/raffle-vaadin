29822f76efd5035462529aabc772f693aac54a66
Otto Ringhofer <otto.ringhofer@gmail.com>
2018-07-03 16:17:23 +0200

    code cleanup, use some Vaadin Flow features

--- src/main/scala/org/scala_vienna/raffle/RaffleComponent.scala
+++ src/main/scala/org/scala_vienna/raffle/RaffleComponent.scala
@@ -1,113 +1,62 @@
 package org.scala_vienna.raffle
 
+import com.vaadin.flow.component.AttachEvent
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.html.{Anchor, H1, H2, Label}
+import com.vaadin.flow.component.listbox.ListBox
 import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.component.orderedlayout.{HorizontalLayout, VerticalLayout}
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.data.provider.{DataProvider, ListDataProvider}
 import org.scala_vienna.raffle.RaffleServer._
-import org.vaadin.addons.vaactor.Vaactor
+import org.vaadin.addons.vaactor.{Vaactor, VaactorSession}
 
 import scala.collection.JavaConverters._
-
 
 class RaffleComponent(title: String)
   extends VerticalLayout with Vaactor.HasActor with Vaactor.SubscribeSession {
 
-  /** This component's name if participating */
-  var myName: Option[String] = None
-
   /** Contains list of raffle participants */
   val participantsList = new java.util.ArrayList[String]()
   val participantsDataProvider: ListDataProvider[String] = DataProvider.ofCollection[String](participantsList)
-  val participantWidth = "200px"
 
   /** Contains participant name */
   val participantName = new TextField("Name:")
-  participantName.setWidth(participantWidth)
-
-  /* todo - Shortcut Handling?
-  val enterListener: ShortcutListener = new ShortcutListener("Submit", ShortcutAction.KeyCode.ENTER, Array.empty[Int]: _*) {
-    override def handleAction(sender: scala.Any, target: scala.Any): Unit = {
-      enterButton.click()
-    }
-  }
-
-  private var registration: Option[Registration] = None
-
-  participantName.addBlurListener((_: FieldEvents.BlurEvent) => {
-    for (r <- registration) r.remove()
-    registration = None
-  })
-
-  participantName.addFocusListener((_: FieldEvents.FocusEvent) => {
-    if (registration.isEmpty)
-      registration = Some(participantName.addShortcutListener(enterListener))
-  })
-  */
-
+  participantName.addValueChangeListener(_ => enterButton.click())
   participantName.focus()
 
-  val participantNameLabel = new Label("")
-  participantNameLabel.setWidth(participantWidth)
+  val participantNameLabel = new Label()
   participantNameLabel.setVisible(false)
 
-  val enterButton = new Button("Enter Raffle", _ => {
-    if (participantName.getValue == "raffle2018coord") {
+  val enterButton = new Button("Enter Raffle", _ =>
+    if (participantName.getValue == "raffle2018coord")
       RaffleServer.raffleServer ! Coordinate(session)
-    }
-    else {
+    else
       RaffleServer.raffleServer ! Participate(participantName.getValue, session)
-    }
-  })
+  )
 
-  val leaveButton = new Button("Leave Raffle", _ => {
-    RaffleServer.raffleServer ! Leave(session)
-  })
+  val leaveButton = new Button("Leave Raffle", _ => RaffleServer.raffleServer ! Leave(session))
   leaveButton.setVisible(false)
 
-  val enterPanel: HorizontalLayout = new HorizontalLayout {
-    setSpacing(true)
-    add(
-      participantName,
-      participantNameLabel,
-      enterButton,
-      leaveButton)
-    /* todo - FlexLayout ?
-    setComponentAlignment(participantName, Alignment.BOTTOM_LEFT)
-    setComponentAlignment(participantNameLabel, Alignment.BOTTOM_LEFT)
-    setComponentAlignment(enterButton, Alignment.BOTTOM_LEFT)
-    setComponentAlignment(leaveButton, Alignment.BOTTOM_LEFT)
-    */
-  }
+  val enterPanel = new HorizontalLayout(
+    participantName,
+    participantNameLabel,
+    enterButton,
+    leaveButton
+  )
 
-  val participantsPanel: Grid[String] = new Grid[String]() {
-    setWidth(participantWidth)
-    addColumn(s => s).setHeader("Participants:")
-    setDataProvider(participantsDataProvider)
-  }
+  val participantsPanel = new ListBox[String]()
+  participantsPanel.setDataProvider(participantsDataProvider)
 
-  val startButton = new Button("Start", _ => {
-    RaffleServer.raffleServer ! StartRaffle
-  })
+  val startButton = new Button("Start", _ => RaffleServer.raffleServer ! StartRaffle)
   startButton.setVisible(false)
   startButton.setEnabled(false)
 
-  val removeButton = new Button("Remove", _ => {
-    val javaArray: Array[String] = participantsPanel.getSelectedItems.toArray(new Array[String](0))
-
-    for (selected <- javaArray) {
-      RaffleServer.raffleServer ! Remove(selected)
-    }
-  })
+  val removeButton = new Button("Remove", _ => RaffleServer.raffleServer ! Remove(participantsPanel.getValue))
   removeButton.setVisible(false)
   removeButton.setEnabled(false)
 
-  val removeAllButton = new Button("Remove All", _ => {
-    RaffleServer.raffleServer ! RemoveAll
-  })
+  val removeAllButton = new Button("Remove All", _ => RaffleServer.raffleServer ! RemoveAll)
   removeAllButton.setVisible(false)
   removeAllButton.setEnabled(false)
 
@@ -126,11 +75,16 @@ class RaffleComponent(title: String)
     removeButton,
     removeAllButton,
     winnerLabel,
-    footer)
+    footer
+  )
 
-  // todo - wait for attach!!! - session ! VaactorSession.RequestSessionState
-  RaffleServer.raffleServer ! GetParticipants
-  RaffleServer.raffleServer ! GetWinner
+  // MUST NOT access session or receive messages before attach!
+  override def onAttach(attachEvent: AttachEvent): Unit = {
+    super.onAttach(attachEvent)
+    session ! VaactorSession.RequestSessionState
+    RaffleServer.raffleServer ! GetParticipants
+    RaffleServer.raffleServer ! GetWinner
+  }
 
   /** Receive function, is called in context of VaadinUI (via ui.access) */
   override def receive: PartialFunction[Any, Unit] = {
@@ -138,7 +92,7 @@ class RaffleComponent(title: String)
       enterButton.setVisible(false)
       leaveButton.setVisible(true)
       participantName.setVisible(false)
-      participantNameLabel.setText(s"Good luck, <b>$name</b>!")
+      participantNameLabel.setText(s"Good luck, $name!")
       participantNameLabel.setVisible(true)
 
     case SessionState.Coordinator =>
